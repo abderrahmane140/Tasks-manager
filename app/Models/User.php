a8dcd00d7cf48452ed093002d03dfc27f72e876f
abderrahmane140 <absarabdo4@gmail.com>
2025-02-21 19:55:38 +0100

    create the the model and migration for the role and tasks

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,11 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+    public function role () {
+        return $this->belongsTo(Role::class);
+    }
+
+    public function tasks () {
+        return $this->hasMany(Task::class);
+    }
 }
